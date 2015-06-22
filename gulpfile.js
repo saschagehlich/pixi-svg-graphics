7d0aec4b5848b00dfb804a191c1118f7221e66ca
Sascha Gehlich <sascha@gehlich.us>
2015-06-22 15:06:45 +0200

    Fix gulpfile codestyle, update pixi

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
-'use strict';
-var path = require('path');
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');;
+var path = require('path')
+var gulp = require('gulp')
+var browserify = require('browserify')
+var source = require('vinyl-source-stream')
 
 gulp.task('default', function () {
   var b = browserify({
     entries: './src/pixi-svg-graphics',
-  });
+  })
 
   return b.bundle()
     .pipe(source('pixi-svg-graphics.js'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist'))
 });
